c814c94fb039f89e94388ffbfe5079b0ebab1c4b
Juliya Smikova <boddxi@yandex.ru>
2025-11-21 20:27:56 +0300

    new commit

--- test_API.py
+++ test_API.py
@@ -1,16 +1,25 @@
 import requests
 
-#Тест-кейс 1: Создание избранного места с корректно заполненными обязательными полями
+
+def get_session_token():
+    response = requests.post('https://regions-test.2gis.com/v1/auth/tokens')
+    token = response.cookies.get('token')
+    return token
+
+
+# Тест-кейс 1: Создание избранного места с корректно заполненными обязательными полями
 def test_case_1():
-    body = {
+    token = get_session_token()
+
+    data = {
         "title": "Тестовое место",
         "lat": 41.89193,
         "lon": 12.51133
     }
-    headers = {'Content-Type': 'application/json', 'Cookie': 'f3a7d0ba35e94caeabf22554dfdb2e3e'}
+    headers = {'Cookie': f'token={token}'}
     response = requests.post(
         'https://regions-test.2gis.com/v1/favorites',
-        json=body,
+        data=data,
         headers=headers
     )
     assert response.status_code == 200, f"Ожидался статус 200, но получили {response.status_code}"
@@ -21,24 +30,87 @@ def test_case_1():
     for field in required_fields:
         assert field in response_data, f"Отсутствует поле: {field}"
 
-    assert response_data['title'] == body['title']
-    assert response_data['lat'] == body['lat']
-    assert response_data['lon'] == body['lon']
+    assert response_data['title'] == data['title']
+    assert response_data['lat'] == data['lat']
+    assert response_data['lon'] == data['lon']
     assert response_data['color'] is None
     assert response_data['id'] > 0
 
     created_at = response_data['created_at']
-    parts = created_at.split(' ')
+
+    assert 'T' in created_at, f"Неверный формат даты: {created_at}"
+    assert ':' in created_at, f"Неверный формат времени: {created_at}"
+
+    parts = created_at.split('T')
 
     assert len(parts) == 2, f"Неверный формат даты"
 
-    date_parts = parts[0].split('-')
-    time_parts = parts[1].split(':')
+    date_part = parts[0]
+    time_part = parts[1]
 
-    assert all(part.isdigit() for part in date_parts), f"Дата должна содержать только цифры: {parts[0]}"
-    assert all(part.isdigit() for part in time_parts), f"Время должно содержать только цифры: {parts[1]}"
+    date_parts = date_part.split('-')
+    assert len(date_parts) == 3, f"Неверный формат даты: {date_part}"
+    assert all(part.isdigit() for part in date_parts), f"Дата должна содержать только цифры: {date_part}"
 
-    #Тест-кейс 2: Создание избранного места с указанием всех допустимых цветов (по очереди)
+    time_part, tz_part = time_part.split('+')
+    time_part = time_part.split(':')
+    tz_part = tz_part.split(':')
+    assert len(time_part) == 3, f"Неверный формат времени: {time_part}"
+    assert all(part.isdigit() for part in time_part), f"Время должно содержать только цифры: {time_part}"
+    assert len(tz_part) == 2, f"Неверный формат часового пояса: {tz_part}"
+    assert all(part.isdigit() for part in tz_part), f"Часовой пояс доджен содержать только цифры: {tz_part}"
 
 
+# Тест-кейс 2: Создание избранного места с указанием всех допустимых цветов (по очереди)
+def test_case_2():
+    token = get_session_token()
+    data = {
+        "title": "Место с цветом",
+        "lat": 45.46427,
+        "lon": 9.18951,
+        "color": "YELLOW"
+    }
+    headers = {'Cookie': f'token={token}'}
+    response = requests.post(
+        'https://regions-test.2gis.com/v1/favorites',
+        data=data,
+        headers=headers
+    )
+    assert response.status_code == 200, f"Ожидался статус 200, но получили {response.status_code}"
+
+    response_data = response.json()
+    required_fields = ['id', 'title', 'lat', 'lon', 'color', 'created_at']
+
+    for field in required_fields:
+        assert field in response_data, f"Отсутствует поле: {field}"
+
+    assert response_data['title'] == data['title']
+    assert response_data['lat'] == data['lat']
+    assert response_data['lon'] == data['lon']
+    assert response_data['color'] == data['color']
+    assert response_data['id'] > 0
+
+    created_at = response_data['created_at']
+
+    assert 'T' in created_at, f"Неверный формат даты: {created_at}"
+    assert ':' in created_at, f"Неверный формат времени: {created_at}"
+
+    parts = created_at.split('T')
+
+    assert len(parts) == 2, f"Неверный формат даты"
+
+    date_part = parts[0]
+    time_part = parts[1]
+
+    date_parts = date_part.split('-')
+    assert len(date_parts) == 3, f"Неверный формат даты: {date_part}"
+    assert all(part.isdigit() for part in date_parts), f"Дата должна содержать только цифры: {date_part}"
+
+    time_part, tz_part = time_part.split('+')
+    time_part = time_part.split(':')
+    tz_part = tz_part.split(':')
+    assert len(time_part) == 3, f"Неверный формат времени: {time_part}"
+    assert all(part.isdigit() for part in time_part), f"Время должно содержать только цифры: {time_part}"
+    assert len(tz_part) == 2, f"Неверный формат часового пояса: {tz_part}"
+    assert all(part.isdigit() for part in tz_part), f"Часовой пояс доджен содержать только цифры: {tz_part}"
 
